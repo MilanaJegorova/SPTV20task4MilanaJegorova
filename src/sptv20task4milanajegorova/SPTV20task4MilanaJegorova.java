9b7b6d6b08fb7fd40f0d10646069b3c86752456e
Milana Jegorova <Milana.Jegorova@ivkhk.ee>
2022-01-25 21:38:31 +0200

    fourth task is done

---
+++ src/sptv20task4milanajegorova/SPTV20task4MilanaJegorova.java
@@ -0,0 +1,31 @@
+package sptv20task4milanajegorova;
+
+import java.util.Arrays;
+
+public class SPTV20task4MilanaJegorova {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int[] number = new int[100];
+        for(int i = 0; i < number.length; i++){
+            while(true){
+                int random = (int)Math.round(Math.random() * 100);
+                if(random%2 != 0){
+                    number[i] = random;
+                    sum += random;
+                    break;
+                }
+
+            }
+        }
+        Arrays.sort(number);
+        for (int s = 0; s < 10; s++){
+            for (int v =0; v < 10; v++){
+                System.out.print(number[s * 10 + v]+" ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("Среднее арифметическое: " + sum/100);
+    }
+    
+}
